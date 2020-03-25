28e9b14fd9fe584091ffa6e3988c3201f0b6d836
Luigi Selmi <luigiselmi@gmail.com>
2020-03-25 11:09:23 +0100

    binomial distribution and grid approximation

---
+++ r/dbinom_grid.R
@@ -0,0 +1,19 @@
+# define grid
+p_grid <- seq(from = 0, to = 1, length.out = 20)
+
+# define prior
+prior <- rep(1, 20)
+
+# compute likelihood at each value in grid
+likelihood <-dbinom(6, size = 9, prob = p_grid)
+
+# compute product of likelihood and prior
+unstd.posterior <- likelihood * prior
+
+# standardize the posterior, so it sums to 1
+posterior <- unstd.posterior / sum(unstd.posterior)
+
+# plot the posterior distribution
+plot(p_grid, posterior, type = 'b', 
+     xlab = 'probability of water', ylab = 'posterior probability')
+mtext('20 poits')
